3f928037302c8845c108fc91b5e7c039c8567ab6
wavyStructures <anja_schwab@gmx.de>
2024-12-04 22:24:18 +0100

    calendar-grid

--- src/app/adventskalender/adventskalender.component.ts
+++ src/app/adventskalender/adventskalender.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-adventskalender',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './adventskalender.component.html',
-  styleUrl: './adventskalender.component.scss'
+  styleUrl: './adventskalender.component.scss',
 })
 export class AdventskalenderComponent {
+  doors = Array.from({ length: 24 }, (_, i) => ({
+    day: i + 1,
+    open: false,
+    content: `Türchen ${i + 1}`,
+  }));
 
+  today = new Date().getDate();
+
+  openDoor(door: any, index: number) {
+    if (door.opened) return;
+    door.opened = true;
+    console.log(`Door ${index + 1} opened!`);
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdventskalenderComponent } from "./adventskalender/adventskalender.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AdventskalenderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
